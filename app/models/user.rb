bcfc5f4c6c72c050a4315dbb2134d5447a6811d7
chimi555 <chinami.s515@gmail.com>
2020-03-27 02:14:11 +0900

    名前かメールアドレスどちらでもログインできるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+  attr_accessor :login
+
+  def login
+    @login || self.name || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).
+        where(['lower(name) = :value OR lower(email) = :value', { value: login.downcase }]).
+        first
+    elsif conditions.key?(:name) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get '/signup', to: 'users/registrations#new'
+    get '/login', to: 'devise/sessions#new'
   end
 end
