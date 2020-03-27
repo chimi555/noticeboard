304179aa9f86a4a9f6a6239437499be00aaf5233
chimi555 <chinami.s515@gmail.com>
2020-03-27 19:54:48 +0900

    ログイン条件をメールアドレスまたは名前からメールアドレスに戻す

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         authentication_keys: [:login]
-  # 追加のattribute
-  attr_accessor :login
+         :recoverable, :rememberable, :validatable
   # バリデーション
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, uniqueness: true, presence: true
-
-  def login
-    @login || name || email
-  end
-
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).
-        where(['lower(name) = :value OR lower(email) = :value', { value: login.downcase }]).
-        first
-    elsif conditions.key?(:name) || conditions.key?(:email)
-      where(conditions.to_h).first
-    end
-  end
 end
