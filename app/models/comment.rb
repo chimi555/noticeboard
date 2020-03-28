6e9adc0ff537d7c19d120593615f69fa3ca9984d
chimi555 <chinami.s515@gmail.com>
2020-03-28 22:25:41 +0900

    commentモデルにバリデーション追加、モデルスペック追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
+  # モデルの関連定義
   belongs_to :user
   belongs_to :topic
+  # バリデーション
+  validates :content, presence: true, length: { maximum: 500 }
+  validates :user_id, presence: true
+  validates :topic_id, presence: true
 end
