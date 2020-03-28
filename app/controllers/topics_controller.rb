5c1b294643b59ce9d9fb7e5efdbadf8d2fe94c07
chimi555 <chinami.s515@gmail.com>
2020-03-28 17:04:51 +0900

    topic作成機能追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,28 @@
 class TopicsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:create, :new]
+
   def show
     @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = current_user.topics.build(topic_params)
+    if @topic.save
+      flash[:success] = '新しいトピックが登録されました'
+      redirect_to topic_path(@topic.id)
+    else
+      flash[:danger] = '新しいトピックの登録に失敗しました'
+      render 'new'
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title, :description)
   end
 end
