a535995f4a7d1caf0802a17354ac87f8242cad58
chimi555 <chinami.s515@gmail.com>
2020-03-28 13:37:33 +0900

    topicモデルにバリデーション追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,6 @@ class Topic < ApplicationRecord
   belongs_to :user
   # バリデーション
   validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, length: { maximum: 200 }
 end
