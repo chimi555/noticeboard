ffc66e4df38fd60d43680aef0fda49d77b66b7e9
chimi555 <chinami.s515@gmail.com>
2020-03-28 21:07:20 +0900

    homeページにトピック一覧を表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @topics = Topic.all.page(params[:page]).per(MAX_OF_DISPLAY_RECENT_TOPICS)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  MAX_OF_DISPLAY_RECENT_TOPICS = 10
+
   def show
     @user = User.find(params[:id])
     @topics = @user.topics.page(params[:page]).per(MAX_OF_DISPLAY_RECENT_TOPICS)
