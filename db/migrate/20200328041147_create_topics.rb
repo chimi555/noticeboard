76402e35bbb1b3cc0adc8904f396ae630c7b5c00
chimi555 <chinami.s515@gmail.com>
2020-03-28 13:15:09 +0900

    topicモデルにdescriptionカラム追加

--- db/migrate/20200328041147_create_topics.rb
+++ db/migrate/20200328041147_create_topics.rb
@@ -2,6 +2,7 @@ class CreateTopics < ActiveRecord::Migration[5.2]
   def change
     create_table :topics do |t|
       t.string :title
+      t.text :description
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_033845) do
+ActiveRecord::Schema.define(version: 2020_03_28_041147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
