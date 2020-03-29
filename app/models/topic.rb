ad831ed11ff61682aa61f0938c9aed1355370263
chimi555 <chinami.s515@gmail.com>
2020-03-29 19:38:39 +0900

    topicモデルにカテゴリー更新のためのsave_categoriesメソッド追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -8,4 +8,20 @@ class Topic < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 200 }
+
+  # カテゴリー更新処理
+  def save_categories(categories)
+    current_categories = self.categories.pluck(:category_name) unless self.categories.nil?
+    old_categories = current_categories - categories
+    new_categories = categories - current_categories
+
+    old_categories.each do |old_name|
+      self.categories.delete Category.find_by(category_name: old_name)
+    end
+
+    new_categories.each do |new_name|
+      topic_category = Category.find_or_create_by(category_name: new_name)
+      self.categories << topic_category
+    end
+  end
 end
