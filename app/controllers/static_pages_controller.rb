add55e11856eb7cff2e3495be5ea6cc73ffaa665
chimi555 <chinami.s515@gmail.com>
2020-03-30 19:27:42 +0900

    N+1問題回避

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @topics = Topic.all.page(params[:page]).per(MAX_OF_DISPLAY)
+    @topics = Topic.all.includes([:categories, :topic_categories]).page(params[:page]).per(MAX_OF_DISPLAY)
     @categories = Category.all
   end
 end
